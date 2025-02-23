f2fe882ebd92c9a1f84a87f276ca4001cb72c6d0
JuliaPalna <palna.julia@gmail.com>
2025-02-24 01:25:40 +0300

    eslint для client настройка

--- packages/client/eslint.config.js
+++ packages/client/eslint.config.js
@@ -1,28 +1,60 @@
 import js from "@eslint/js";
 import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
+import eslintReact from "eslint-plugin-react";
+import eslintReactHooks from "eslint-plugin-react-hooks";
+import eslintReactRefresh from "eslint-plugin-react-refresh";
+import prettierPlugin from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
 import tseslint from "typescript-eslint";
 
+/** @type { import("eslint").Linter.Config[] } */
 export default tseslint.config(
-  { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
     plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
+      react: eslintReact,
+      "react-hooks": eslintReactHooks,
+      "react-refresh": eslintReactRefresh,
+      "@typescript-eslint": tseslint.plugin,
+      prettier: prettierPlugin
     },
+  },
+  { 
+    ignores: ["node_modules", "dist","eslint.config.js"] 
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        ...globals.es2020,
+      },
+      parserOptions: {
+        project: ["tsconfig.json", "tsconfig.node.json","tsconfig.app.json"]
+      }
+    },
+  },
+  {
+    files:  ["**/*.{ts,tsx}"],
     rules: {
-      ...reactHooks.configs.recommended.rules,
+      ...eslintConfigPrettier.rules,
+      ...eslintReactHooks.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      "react/jsx-no-target-blank": "off",
+      "prefer-const": "error",
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
       ],
-    },
-  },
-);
+      "@typescript-eslint/strict-boolean-expressions": "off",
+      "@typescript-eslint/prefer-nullish-coalescing": "off",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/restrict-template-expressions": "off",
+      "@typescript-eslint/triple-slash-reference": "off",
+      "@typescript-eslint/ban-types": "off",
+      "@typescript-eslint/consistent-type-assertions": "off",
+      "jsx-a11y/anchor-is-valid": "off"
+    }
+  }
+)
